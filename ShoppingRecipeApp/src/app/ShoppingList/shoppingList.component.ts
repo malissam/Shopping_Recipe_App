37b3d61fa4658f79dded3950acccb083881065f3
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2020-05-25 01:24:24 -0700

    the structure of app, begining of nav bar

--- ShoppingRecipeApp/src/app/ShoppingList/shoppingList.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-shoppinglist',
-    templateUrl: './shoppingList.component.html'
-})
-export class ShoppingListComponent{
-
-}
