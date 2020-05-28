f6bc5925e6e9d2074909f5ec7c24d120211e3a4b
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2020-05-27 21:18:15 -0700

    recipe model and recipe in recipe list

--- ShoppingRecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingRecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test recipe','https://craftycoin.com/wp-content/uploads/2015/04/Recipes-header.png')
+  ];
   constructor() { }
 
   ngOnInit(): void {
