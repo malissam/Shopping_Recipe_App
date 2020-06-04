e44ab78211f954a9533f75fdc17f144886c06507
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2020-06-04 08:39:16 -0700

    fixed the list item component to its proper place

--- ShoppingRecipeApp/src/app/app.module.ts
+++ ShoppingRecipeApp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -18,7 +17,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent
   ],
