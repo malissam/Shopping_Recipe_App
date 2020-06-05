69b4db982c739ec784797283d56dd58cb1d212dc
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2020-06-04 22:01:43 -0700

    recipe component

--- ShoppingRecipeApp/src/app/app.module.ts
+++ ShoppingRecipeApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +26,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
